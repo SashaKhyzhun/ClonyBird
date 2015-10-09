5590233a97e6c6892b14d9901a83e854113c0372
SashaKhyzhun <sasha6427@gmail.com>
2015-10-09 22:20:31 +0300

    ++

---
+++ Assets/GroundMoverBack.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundMoverBack : MonoBehaviour {
+
+    float speed = -0.7f;
+
+    void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+    }
+}
---
+++ Assets/GroundMoverForward.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundMoverForward : MonoBehaviour {
+
+    float speed = -0.1f;
+
+    void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+    }
+}
